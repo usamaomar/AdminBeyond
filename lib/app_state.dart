ca625f2dcfd20eb4f63015e85a5625dc1c806232
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-11 07:37:11 +0000

    Updating to latest FlutterFlow output.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -17,22 +17,55 @@ class FFAppState extends ChangeNotifier {
     _instance = FFAppState._internal();
   }
 
-  Future initializePersistedState() async {}
+  Future initializePersistedState() async {
+    prefs = await SharedPreferences.getInstance();
+    _safeInit(() {
+      if (prefs.containsKey('ff_userModel')) {
+        try {
+          final serializedData = prefs.getString('ff_userModel') ?? '{}';
+          _userModel =
+              UserModelStruct.fromSerializableMap(jsonDecode(serializedData));
+        } catch (e) {
+          print("Can't decode persisted data type. Error: $e.");
+        }
+      }
+    });
+    _safeInit(() {
+      _accessRoleModelList = prefs
+              .getStringList('ff_accessRoleModelList')
+              ?.map((x) {
+                try {
+                  return AccessRoleModelStruct.fromSerializableMap(
+                      jsonDecode(x));
+                } catch (e) {
+                  print("Can't decode persisted data type. Error: $e.");
+                  return null;
+                }
+              })
+              .withoutNulls
+              .toList() ??
+          _accessRoleModelList;
+    });
+  }
 
   void update(VoidCallback callback) {
     callback();
     notifyListeners();
   }
 
+  late SharedPreferences prefs;
+
   UserModelStruct _userModel = UserModelStruct.fromSerializableMap(jsonDecode(
       '{\"userName\":\"\\\"\\\"\",\"id\":\"\\\"\\\"\",\"firstName\":\"\\\"\\\"\",\"lastName\":\"\\\"\\\"\",\"email\":\"\\\"\\\"\",\"isActive\":\"true\",\"emailConfirmed\":\"true\",\"phoneNumber\":\"\\\"\\\"\",\"profilePictureDataUrl\":\"\\\"\\\"\",\"accessRole\":\"0\",\"token\":\"\\\"\\\"\",\"refreshToken\":\"\\\"\\\"\"}'));
   UserModelStruct get userModel => _userModel;
   set userModel(UserModelStruct _value) {
     _userModel = _value;
+    prefs.setString('ff_userModel', _value.serialize());
   }
 
   void updateUserModelStruct(Function(UserModelStruct) updateFn) {
     updateFn(_userModel);
+    prefs.setString('ff_userModel', _userModel.serialize());
   }
 
   List<AccessRoleModelStruct> _accessRoleModelList = [
@@ -48,18 +81,26 @@ class FFAppState extends ChangeNotifier {
   List<AccessRoleModelStruct> get accessRoleModelList => _accessRoleModelList;
   set accessRoleModelList(List<AccessRoleModelStruct> _value) {
     _accessRoleModelList = _value;
+    prefs.setStringList(
+        'ff_accessRoleModelList', _value.map((x) => x.serialize()).toList());
   }
 
   void addToAccessRoleModelList(AccessRoleModelStruct _value) {
     _accessRoleModelList.add(_value);
+    prefs.setStringList('ff_accessRoleModelList',
+        _accessRoleModelList.map((x) => x.serialize()).toList());
   }
 
   void removeFromAccessRoleModelList(AccessRoleModelStruct _value) {
     _accessRoleModelList.remove(_value);
+    prefs.setStringList('ff_accessRoleModelList',
+        _accessRoleModelList.map((x) => x.serialize()).toList());
   }
 
   void removeAtIndexFromAccessRoleModelList(int _index) {
     _accessRoleModelList.removeAt(_index);
+    prefs.setStringList('ff_accessRoleModelList',
+        _accessRoleModelList.map((x) => x.serialize()).toList());
   }
 
   void updateAccessRoleModelListAtIndex(
@@ -67,11 +108,15 @@ class FFAppState extends ChangeNotifier {
     AccessRoleModelStruct Function(AccessRoleModelStruct) updateFn,
   ) {
     _accessRoleModelList[_index] = updateFn(_accessRoleModelList[_index]);
+    prefs.setStringList('ff_accessRoleModelList',
+        _accessRoleModelList.map((x) => x.serialize()).toList());
   }
 
   void insertAtIndexInAccessRoleModelList(
       int _index, AccessRoleModelStruct _value) {
     _accessRoleModelList.insert(_index, _value);
+    prefs.setStringList('ff_accessRoleModelList',
+        _accessRoleModelList.map((x) => x.serialize()).toList());
   }
 }
 
